52b6bb458a7aa010ca95cb30b9b397fdc1ab3593
Pelin <79792586+pelinozdemir@users.noreply.github.com>
2022-07-31 17:24:24 +0300

    Bank Management System with Java

---
+++ Banksystem/JavaApplication1/src/javaapplication1/yonetici/sistemTarih.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1.yonetici;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Pelin
+ */
+public class sistemTarih {
+ int tarih;   
+    
+    public  sistemTarih(){
+        
+    }
+    public  sistemTarih(int tarih){
+        this.tarih=tarih;
+    }
+    public int getTarih(){
+        return this.tarih;
+    }
+}
